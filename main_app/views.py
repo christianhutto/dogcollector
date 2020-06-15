7180aebbfac6cf658ad7dd836fe74bc778aa44a2
chutto <christianhutto@gmail.com>
2020-06-15 18:31:00 -0500

    added dog model, registered model and created detail route

--- main_app/views.py
+++ main_app/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Dog
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 
 def about(request):
-    return render('<h1>About</h1>')
+    return render(request, 'about.html')
 
 def dogs_index(request):
-    return render('<h1>Dogs</h1>')
+    dogs = Dog.objects.all()
+    context = {'dogs': dogs}
+    return render(request, 'dogs/index.html', context)
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    context = {'dog': dog}
+    return render(request, 'dogs/detail.html', context)
